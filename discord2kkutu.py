e4c605ffd8fe7cff197e1c10c6034493203ade7c
Kim Ha Gyun <khk49121@gmail.com>
2018-02-11 13:29:49 +0900

    버그 수정 / Discord Rewrite로 변경
    버그 수정 / Discord Rewrite로 변경

--- discord2kkutu.py
+++ discord2kkutu.py
@@ -1,87 +1,64 @@
-# import kkutu2discord
-import discord
-import json
-import websocket
+#  도움 주신 분들 
+#  Helloyunho / https://github.com/Helloyunho
+#  "펀크" / https://github.com/rlacks628628 
+
+
+# ______ _                       _  _____  _   __ _   __    _____     
+# |  _  (_)                     | |/ __  \| | / /| | / /   |_   _|    
+# | | | |_ ___  ___ ___  _ __ __| |`' / /'| |/ / | |/ / _   _| |_   _ 
+# | | | | / __|/ __/ _ \| '__/ _` |  / /  |    \ |    \| | | | | | | |
+# | |/ /| \__ \ (_| (_) | | | (_| |./ /___| |\  \| |\  \ |_| | | |_| |
+# |___/ |_|___/\___\___/|_|  \__,_|\_____/\_| \_/\_| \_/\__,_\_/\__,_|
+                                                                    
+                                                                                                                                                                                             
+                                                                                                                         
+                                                                                                                         
+import websocket, json
+from collections import OrderedDict
 import asyncio
+import multiprocessing
+import discord
 import threading
-client = discord.Client()
-global socket_url
-global text1
 
-text1 = None
-socket_url = "socket_here"
-def smessage(msg):
-    global ws
-    ws.send('{"type":"talk","value":"'+msg+'"}')
+class discordbot(discord.Client):
+    async def on_ready(self):
+        print('\n\n디스코드 로그인 완료\n\n')
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        ws.send('{"type":"talk","value":"' + '[디코] ' + message.author.name + ': ' + message.content + '"}')
 
-# async def dsmessage(tgmsg):
-#     await client.wait_until_ready()
+async def send_discord(me):
+    ch = client.get_channel(Channel_Id_Here)
+    await ch.send(me)
+    return True
 
-    # global tgmsg
-    # while True:
-    #     if not tgmsg is None:
-    #         await client.send_message(discord.Channel(id="400592573867229185"), tgmsg)
-    #         tgmsg = None
-    #         print(tgmsg)
-    #     else:
-    #         pass
-# async def kkutu():
-async def send():
-    global text1
-    while True:
-        
-        if not text1 == None:
-            ch = discord.Object(id="400592573867229185")
-            await client.send_message(ch, text1)
-            text1 = None
-        else:
-            pass    
-class kkutu(threading.Thread):
+class kkutubot(threading.Thread):
     def run(self):
-        global ws
-
-        def onmessage(ws,message):
-            global text1
-        # print(message)
-            data = json.loads(message)
-            if data["type"] == "chat":
-                try:
-                    name = data["profile"]["title"]
-                except:
-                    name = data["profile"]["name"]
-                    # sendmsg("400592573867229185","test")
-
-                # print(ws,message)
-                if not name == "GUEST8779":
-                    print("[끄투] %s : %s" %(name, data["value"]))
+        def on_message(ws, message):
+            jsonstring = json.loads(message)
+            if jsonstring['type'] == 'chat':
+                if not jsonstring['profile']['id'] == 'ID_here':
+                    print(jsonstring['value'])
                     try:
-                        name = data["profile"]["title"]
+                        name = jsonstring["profile"]["title"]
                     except:
-                        name = data["profile"]["name"]
-                    text3 = "[끄투] %s : %s" %(name, data["value"])
-                    text1 = text3.replace("@everyone","")
-                    text1 = text1.replace("@here","")
+                        name = jsonstring["profile"]["name"]
+
+                    client.loop.create_task(send_discord('[끄투] ' + name + " : " + jsonstring['value']))
+
         def on_close(ws):
-            print("경고! websocket이 중지되었습니다!")
+            print("\n\n 경고! websocket이 종료되었습니다! \n\n")
 
+        socket_url = 'ws://끄투링크:포트'
+        kk = socket_url
+        self.kk = kk.replace('ws://', '')
+        socket_url = socket_url + '/Socket_Here'
         websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(socket_url, on_message=onmessage,on_close=on_close)
+        global ws
+        ws = websocket.WebSocketApp(socket_url, on_message=on_message, on_close=on_close)
         ws.run_forever()
-
-
-@client.event
-async def on_ready():
-    print("\n디스코드 준비 완료!")
-
-@client.event
-async def on_message(message):
-    global ws
-    if message.author.id != "411772883652706304" and message.channel.id == "400592573867229185":
-        print("[디코] " +message.author.name + " : " + message.content)
-
-        smessage("[디코] " +message.author.name + " : " + message.content)
-
-recv = kkutu(name="get_chat")
+recv = kkutubot(name="get_chat") 
 recv.start()
-client.loop.create_task(send())
-client.run("token")
+client = discordbot()
+client.run("Token_Here")
